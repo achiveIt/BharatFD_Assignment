ad75c2ec11d4a31e0ec9a984ac72dfd9e3f11a2d
achiveIt <154082077+achiveIt@users.noreply.github.com>
2025-02-01 13:12:55 +0530

    feat:  feat: Add (delete FAQs) feature for Admins (#8)

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { addFAQ, deleteFAQ } from "../controller/admin.controller.js";
+import { addFAQ, deleteFAQ, editFAQ } from "../controller/admin.controller.js";
 
 const router = Router();
 
 router.route('/faq').post(addFAQ);
 
 router.route('/faq/:id').delete(deleteFAQ);
+
+router.route('/faq/:id').patch(editFAQ);
 
 export default router
