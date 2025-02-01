eb56e99440db9fdfcb6e87c65c5858ca605a9efe
Shivdutt <happyface979797@gmail.com>
2025-02-01 12:33:25 +0530

    feat: Add FAQs features for admins

--- src/app.js
+++ src/app.js
@@ -8,4 +8,8 @@ app.use(express.json({limit:"16kb"}));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
+import AdminRouter from "../src/routes/admin.route.js"
+
+app.use('/api/v1/Admin',AdminRouter)
+
 export {app}
