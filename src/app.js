3c81db312178f36f4da2c48ae7510c84d1db6c15
Shivdutt <happyface979797@gmail.com>
2025-02-02 14:21:41 +0530

    fix: Add Cors options

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+
+const corsOptions = {
+origin: '*',
+methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
+credentials: true,               
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json({limit:"16kb"}));
 
