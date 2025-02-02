d4368e6916fea429c8d3cbfdaeab3fef6bba01bc
achiveIt <154082077+achiveIt@users.noreply.github.com>
2025-02-02 10:59:47 +0530

    feat: Add language translation feature  (#10)

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,23 +1,62 @@
 import { Faq } from '../model/faq.model.js';
 import asyncHandler from '../utils/asyncHandler.js';
 import ApiResponse from '../utils/apiResponse.js';
+import { translate } from '@vitalets/google-translate-api';
 
-const getAllFAQs = asyncHandler(async (req, res) => {
+const translateText =  async(text, lang) =>{
     try {
-        const faqs = await Faq.find().select('-_id'); 
+        const res = await translate(
+            text, 
+            { 
+                to : lang 
+            }
+        );
+
+        return res.text;
+
+      } catch (error) {
+        console.error("Translation Error:", error);
+        return text; 
+      }
+}
+const getFAQs = asyncHandler(async (req, res) => {
+    try {
+        const faqs = await Faq.find().select('-_id');
+        console.log(faqs);
+
+        const lang = req.params.lang || 'en';
+        let translatedFAQs = faqs;
+
+        if (lang !== 'en') {
+            translatedFAQs = await Promise.all(
+                faqs.map(async (faq) => {
+                    const translatedQuestion = await translateText(faq.question, lang);
+                    const translatedAnswer = await translateText(faq.answer, lang);
+                    
+                    return {
+                        question: translatedQuestion,
+                        answer: translatedAnswer,
+                    };
+                })
+            );
+        }
 
         return res
                 .status(200)
                 .json(
-                    new ApiResponse(200, { faqs }, "FAQs fetched successfully")
+                    new ApiResponse(200, translatedFAQs, "FAQs fetched successfully")
                 );
     } catch (error) {
         return res
                 .status(500)
                 .json(
-                    new ApiResponse(500, {}, "Error fetching FAQs", error.message)
-                );
+                new ApiResponse(500, {}, "Error fetching FAQs", error.message)
+            );
     }
 });
 
-export default getAllFAQs;
+
+
+export{
+    getFAQs,
+} 
